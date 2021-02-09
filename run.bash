3eaee2d3d3b1ba6cb23606cca26f4266764d4430
thejauffre <thejauffre@gmail.com>
2021-02-09 11:16:42 +0100

    Updated launch file

--- run.bash
+++ run.bash
@@ -2,4 +2,4 @@
 colcon build
 source ./install/setup.bash
 export GAZEBO_MODEL_PATH=./src/myrobot/models/
-
+rviz2 -d src/myrobot/resource/myrobot2.rviz & ros2 launch myrobot turtlebot3_world.launch.py
